bdf6bdc52f3f78a387ffc2d88091599f9e7f2a6c
lydia <joyplay00@hotmail.com>
2021-02-20 18:14:21 -0600

    test commit from AS

--- TeamCode/src/main/java/discoduckbots/hardware/Intake.java
+++ TeamCode/src/main/java/discoduckbots/hardware/Intake.java
@@ -14,6 +14,7 @@ public class Intake {
     public Intake(DcMotor intakeMotor, Servo intakePusher) {
         this.intakeMotor = intakeMotor;
         this.intakePusher = intakePusher;
+        
     }
 
     public void intake() {
